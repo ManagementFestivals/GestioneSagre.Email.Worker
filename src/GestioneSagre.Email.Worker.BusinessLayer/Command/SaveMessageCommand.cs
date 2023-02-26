decd6b50a4c2345f8d9d99c851257933a44ac8f1
Angelo Pirola <angelo9871@gmail.com>
2023-02-26 20:01:12 +0100

    Implementazione MediatR Command

---
+++ src/GestioneSagre.Email.Worker.BusinessLayer/Command/SaveMessageCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace GestioneSagre.Email.Worker.BusinessLayer.Command;
+
+public class SaveMessageCommand : IRequest<EmailMessage>
+{
+    public Guid MessageId { get; set; }
+    public string Recipient { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public int SenderCount { get; set; }
+    public MailStatus Status { get; set; }
+
+    public SaveMessageCommand(MessageSaveRequest inputModel)
+    {
+        MessageId = inputModel.MessageId;
+        Recipient = inputModel.Recipient;
+        Subject = inputModel.Subject;
+        Message = inputModel.Message;
+        SenderCount = inputModel.SenderCount;
+        Status = inputModel.Status;
+    }
+}
